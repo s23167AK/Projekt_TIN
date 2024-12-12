f42c465b8e57f1d0f39d0bdd764bf20c070ddae0
Ola1 <s23068@pjwstk.edu.pl>
2024-12-12 21:23:44 +0100

    zmiany w walidacji i w widokach

--- public/js/validateBookForm.js
+++ public/js/validateBookForm.js
@@ -1,76 +1,97 @@
-function validateBookForm() {
-    const titleInput = document.getElementById("title");
-    const authorInput = document.getElementById("author");
-    const yearInput = document.getElementById("year");
-    const genreInput = document.getElementById("genre");
-    const descriptionInput = document.getElementById("description");
+function validateForm() {
+    let isValid = true;
+    let errorSummary = "";
 
+    const title = document.getElementById("title").value.trim();
     const errorTitle = document.getElementById("errorTitle");
+    const author = document.getElementById("author").value.trim();
     const errorAuthor = document.getElementById("errorAuthor");
+    const year = document.getElementById("year").value.trim();
     const errorYear = document.getElementById("errorYear");
+    const genre = document.getElementById("genre").value.trim();
     const errorGenre = document.getElementById("errorGenre");
+    const description = document.getElementById("description").value.trim();
     const errorDescription = document.getElementById("errorDescription");
 
-    const errorsSummary = document.getElementById("errorsSummary");
+    const emojiPattern = /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]+/u;
+    const digitsPattern = /\d/;
+    const specialCharsPattern = /[^a-zA-Z0-9\s.-]/;
+    const yearPattern = /^\d{1,4}$/;
 
-    resetErrors(
-        [titleInput, authorInput, yearInput, genreInput, descriptionInput],
-        [errorTitle, errorAuthor, errorYear, errorGenre, errorDescription],
-        errorsSummary
-    );
+    errorTitle.textContent = "";
+    errorAuthor.textContent = "";
+    errorYear.textContent = "";
+    errorGenre.textContent = "";
+    errorDescription.textContent = "";
 
-    let valid = true;
-
-    if (!checkRequired(titleInput.value)) {
-        valid = false;
-        titleInput.classList.add("error-input");
-        errorTitle.innerText = "Pole 'Tytuł' jest wymagane";
-    } else if (!checkTextLenghtRange(titleInput.value, 2, 255)) {
-        valid = false;
-        titleInput.classList.add("error-input");
-        errorTitle.innerText = "Pole 'Tytuł' powinno zawierać od 2 do 255 znaków";
+    if (title.length < 2) {
+        errorTitle.textContent = "Tytuł musi mieć co najmniej 2 znaki.";
+        errorSummary += "Błąd w polu Tytuł.\n";
+        isValid = false;
+    } else if (title.length > 60) {
+        errorTitle.textContent = "Tytuł może mieć maksymalnie 60 znaków.";
+        errorSummary += "Błąd w polu Tytuł.\n";
+        isValid = false;
+    }else if (emojiPattern.test(title)) {
+        errorTitle.textContent = "Tytuł nie może zawierać emotikonów.";
+        errorSummary += "Błąd w polu Tytuł.\n";
+        isValid = false;
     }
 
-    if (!checkRequired(authorInput.value)) {
-        valid = false;
-        authorInput.classList.add("error-input");
-        errorAuthor.innerText = "Pole 'Autor' jest wymagane";
-    } else if (!checkTextLenghtRange(authorInput.value, 2, 255)) {
-        valid = false;
-        authorInput.classList.add("error-input");
-        errorAuthor.innerText = "Pole 'Autor' powinno zawierać od 2 do 255 znaków";
+    if (author.length < 2) {
+        errorAuthor.textContent = "Pole Autor musi mieć co najmniej 2 znaki.";
+        errorSummary += "Błąd w polu Autor.\n";
+        isValid = false;
+    } else if (author.length > 60) {
+        errorAuthor.textContent = "Pole Autor może mieć maksymalnie 60 znaków.";
+        errorSummary += "Błąd w polu Autor.\n";
+        isValid = false;
+    } else if (emojiPattern.test(author)) {
+        errorAuthor.textContent = "Pole Autor nie może zawierać emotikonów.";
+        errorSummary += "Błąd w polu Autor.\n";
+        isValid = false;
+    } else if (digitsPattern.test(author)) {
+        errorAuthor.textContent = "Pole Autor nie może zawierać cyfr.";
+        errorSummary += "Błąd w polu Autor.\n";
+        isValid = false;
+    }
+    if (!year) {
+        errorYear.textContent = "Pole Rok publikacji jest wymagane.";
+        errorSummary += "Błąd w polu Rok publikacji.\n";
+        isValid = false;
+    } else if (isNaN(year)) {
+        errorYear.textContent = "Pole Rok musi zawierać liczbę.";
+        errorSummary += "Błąd w polu Rok publikacji.\n";
+        isValid = false;
+    } else if (!yearPattern.test(year)) {
+        errorYear.textContent = "Rok publiikacju musi być liczbą całkowitą od 0 do 9999.";
+        errorSummary += "Błąd w polu Rok publikacji.\n";
+        isValid = false;
+    }else if(specialCharsPattern.test(year)){
+        errorYear.textContent = "Rok publikacji nie może zawierać znaków specjalnych.";
+        errorSummary += "Błąd w polu Rok publikacji.\n";
+        isValid = false;
+    }
+    if (genre === "") {
+        errorGenre.textContent = "Wybierz odpowiedni gatunek książki.";
+        errorSummary += "Błąd w polu Gatunek.\n";
+        isValid = false;
+    }
+    if (description.length < 10) {
+        errorDescription.textContent = "Opis musi mieć co najmniej 10 znaków.";
+        errorSummary += "Błąd w polu Opis.\n";
+        isValid = false;
+    } else if (description.length > 500) {
+        errorDescription.textContent = "Opis może mieć maksymalnie 500 znaków.";
+        errorSummary += "Błąd w polu Opis.\n";
+        isValid = false;
+    } else if (emojiPattern.test(description)) {
+        errorDescription.textContent = "Opis nie może zawierać emotikonów.";
+        errorSummary += "Błąd w polu Opis.\n";
+        isValid = false;
     }
 
-    const currentYear = new Date().getFullYear();
-    if (!checkRequired(yearInput.value)) {
-        valid = false;
-        yearInput.classList.add("error-input");
-        errorYear.innerText = "Pole 'Rok publikacji' jest wymagane";
-    } else if (!checkNumber(yearInput.value)) {
-        valid = false;
-        yearInput.classList.add("error-input");
-        errorYear.innerText = "Pole 'Rok publikacji' musi być liczbą całkowitą";
-    } else if (!checkNumberRange(yearInput.value, 1000, currentYear)) {
-        valid = false;
-        yearInput.classList.add("error-input");
-        errorYear.innerText = `Pole 'Rok publikacji' musi być liczbą pomiędzy 1000 a ${currentYear}`;
-    }
+    document.getElementById("errorSummary").innerHTML = errorSummary.replace(/\n/g, "<br>");
 
-    if (!checkRequired(genreInput.value)) {
-        valid = false;
-        genreInput.classList.add("error-input");
-        errorGenre.innerText = "Pole 'Gatunek' jest wymagane";
-    }
-
-    if (!checkTextLenghtRange(descriptionInput.value, 0, 1000)) {
-        valid = false;
-        descriptionInput.classList.add("error-input");
-        errorDescription.innerText = "Pole 'Opis' nie może przekraczać 1000 znaków";
-    }
-
-    if (!valid) {
-        errorsSummary.innerText = "Formularz zawiera błędy";
-    }
-
-    return valid;
+    return isValid;
 }
